2a70a204557624e1d482e852f7ead6d492b03bf3
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-12-28 22:50:18 -0300

    submetendo projeto ao meu repositorio particular

---
+++ src/Components/DarkMode.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../SASS/Filter.sass';
+
+export default function DarkMode() {
+  return (
+    <div className="buttonDark">
+      <button type="button">dark</button>
+    </div>
+  );
+}
